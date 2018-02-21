ada662084dd77e438ca04b3e97d3a64accc6137b
Matt Ha <matthewha1993@gmail.com>
2018-02-21 10:08:58 -0800

    Change buy price in api call to spot price

--- src/components/main_view_container.jsx
+++ src/components/main_view_container.jsx
@@ -1,6 +1,8 @@
 import {connect} from 'react-redux';
 import {withRouter} from 'react-router';
 
+import {requestCoinPrice} from '../actions/coin_actions';
+
 // Components
 import MainView from './main_view';
 
@@ -9,6 +11,7 @@ const mapStateToProps = (state, ownProps) => ({
 })
 
 const mapDispatchToProps = (dispatch, ownProps) => ({
+  requestCoinPrice: (coin_tag) => dispatch(requestCoinPrice(coin_tag))
 })
 
 
--- src/util/coin_api_util.jsx
+++ src/util/coin_api_util.jsx
@@ -1,7 +1,7 @@
 export const fetchCoinPrice = (coin_tag) => (
   $.ajax({
     type: 'GET',
-    url: `https://api.coinbase.com/v2/prices/${coin_tag}-USD/buy`,
+    url: `https://api.coinbase.com/v2/prices/${coin_tag}-USD/spot`,
     dataType: 'json',
   });
 );
