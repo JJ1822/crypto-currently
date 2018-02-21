43f2f18f81ee490fe22283f5ff4013825e69fbde
Matt Ha <matthewha1993@gmail.com>
2018-02-21 09:20:55 -0800

    Rename requestCoin to requestCoinPrice

--- src/actions/coin_actions.js
+++ src/actions/coin_actions.js
@@ -1,5 +1,13 @@
-// import * as CoinAPIUtil from '../util/coin_api_util';
-//
-// export const requestCoin = () => (
-//   CoinAPIUtil.fetchCoin(coin_tag).then(payload => dispatch(receiveCoin(payload)))
-// )
+import * as CoinAPIUtil from '../util/coin_api_util';
+
+
+export const RECEIVE_COIN_PRICE = 'RECEIVE_COIN_PRICE';
+
+export const requestCoinPrice = () => (
+  CoinAPIUtil.fetchCoinPrice(coin_tag).then(payload => dispatch(receiveCoin(payload)))
+);
+
+const receiveCoinPrice = (payload) => ({
+  type: RECEIVE_COIN_PRICE,
+  payload,
+});
--- src/util/coin_api_util.jsx
+++ src/util/coin_api_util.jsx
@@ -1,4 +1,4 @@
-export const fetchCoin = (coin_tag) => (
+export const fetchCoinPrice = (coin_tag) => (
   $.ajax({
     type: 'GET',
     url: `https://api.coinbase.com/v2/prices/${coin_tag}-USD/buy`,
