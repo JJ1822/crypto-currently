7dc62bbba80b1c2b92df71e3f5d60021c3b36b18
Matt Ha <matthewha1993@gmail.com>
2018-02-21 09:59:48 -0800

    Create coin reducer switch case statement

---
+++ src/reducers/coin_reducer.js
@@ -0,0 +1,17 @@
+import {merge} from 'lodash';
+
+import {RECEIVE_COIN_PRICE} from '../actions/coin_actions';
+
+
+const coinReducer = (state = {}, action) => {
+  Object.freeze(oldState);
+
+  switch(action.type) {
+    case 'RECEIVE_COIN_PRICE':
+      return merge({}, action.payload.data);
+    default:
+      return state;
+  }
+};
+
+export default coinReducer;
