f8efc98b130227bd21cf77eaff06179298f1dc78
fmbf <circumnavigate.the.globe@gmail.com>
2018-02-23 11:49:37 -0800

    keys stuff

--- src/util/twitter.js
+++ src/util/twitter.js
@@ -3,10 +3,10 @@ var Twitter = require('twitter');
 class StreamTwitter {
   constructor() {
     const params = {
-      consumer_key: 'FlbMS4GcrGnRZlNn9HX23nHhB',
-      consumer_secret: '3fOR4yIBQjiSdTsUGMpnkMkI0NDyOyapAorjAZkcbxUaiUkS4B',
-      access_token_key: '965675476181164032-57mbhUwoo7Dfz3tcQybt4N2TZCVHtQt',
-      access_token_secret: 'T2FY3HP3cOEDk6Z2CTEkceMFYDb4466Bd8LxZnklKxuU3'
+      consumer_key: 'xx',
+      consumer_secret: 'xx',
+      access_token_key: 'xx-xx',
+      access_token_secret: 'xx'
     }
     this.client = new Twitter(params);
     this.stream = this.client.stream('statuses/filter', {track: 'bitcoin, btc, crypto, cryptocurrency, blockchain, bitcoins, bitcointalk'});
