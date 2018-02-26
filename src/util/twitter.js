fadc94713afa40ede9a5999755abb25bd840bb00
fmbf <circumnavigate.the.globe@gmail.com>
2018-02-26 12:56:05 -0800

    changes for heroku test 4

---
+++ src/util/twitter.js
@@ -0,0 +1,20 @@
+var Twitter = require('twitter');
+
+class StreamTwitter {
+constructor() {
+  const params = require('../../twitter-geo-server-js-master/twitter-credentials');
+  this.client = new Twitter(params.watsonKey);
+  this.stream = this.client.stream('statuses/filter', {track: 'bitcoin, btc'});
+}
+
+tweetStream(callback) {
+  this.stream.on('error', function(error) {
+    console.log(error);
+  });
+
+  return this.stream.on('data', callback)
+}
+
+}
+
+module.exports = StreamTwitter;
