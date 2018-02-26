dca374cb2c711e815e849e02f79d3dfa86098e0f
Jay Johnson <jaysbeverage@gmail.com>
2018-02-25 21:37:54 -0800

    hide keys

--- src/util/twitter.js
+++ src/util/twitter.js
@@ -2,13 +2,8 @@ var Twitter = require('twitter');
 
 class StreamTwitter {
   constructor() {
-    const params = {
-      consumer_key: 'FlbMS4GcrGnRZlNn9HX23nHhB',
-      consumer_secret: '3fOR4yIBQjiSdTsUGMpnkMkI0NDyOyapAorjAZkcbxUaiUkS4B',
-      access_token_key: '965675476181164032-57mbhUwoo7Dfz3tcQybt4N2TZCVHtQt',
-      access_token_secret: 'T2FY3HP3cOEDk6Z2CTEkceMFYDb4466Bd8LxZnklKxuU3'
-    }
-    this.client = new Twitter(params);
+    const params = require('../../twitter-geo-server-js-master/twitter-credentials');
+    this.client = new Twitter(params.watsonKey);
     this.stream = this.client.stream('statuses/filter', {track: 'bitcoin, btc'});
   }
 
