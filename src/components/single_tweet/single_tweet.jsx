fbf81d3250b10af081e44e8bcb0cf557ec75b232
fmbf <circumnavigate.the.globe@gmail.com>
2018-02-25 16:23:30 -0800

    add back in main tweet object

--- src/components/single_tweet/single_tweet.jsx
+++ src/components/single_tweet/single_tweet.jsx
@@ -16,11 +16,26 @@ constructor(props) {
   render() {
     return (
       <div className='main-tweet-box'>
-        <div className='main-tweet-simple'>
-          <p>
-            " {`${this.simpleTweet}`} "
-          </p>
-        </div>
+          <blockquote className="twitter-tweet"
+            data-cards="hidden"
+            data-width="100%"
+            data-height="100%"
+            data-theme="dark"
+            data-lang="en">
+            <a href="https://twitter.com/ForbesCrypto/status/967442398891651073?ref_src=twsrc%5Etfw"></a>
+          {
+            !function(d,s,id){
+              var js, fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';
+              // console.log(s);
+
+              if(!d.getElementById(id)){
+                js=d.createElement(s);js.id=id;
+                js.src=p+"://platform.twitter.com/widgets.js";
+                fjs.parentNode.insertBefore(js,fjs);
+              }
+            }(document,"script","twitter-wjs")
+          }
+          </blockquote>
       </div>
 
 
@@ -59,3 +74,10 @@ export default SingleTweet;
 
 
 // <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+
+
+
+//
+// <p>
+//   " {`${this.simpleTweet}`} "
+// </p>
